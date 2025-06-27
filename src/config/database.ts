b06cce5a8a83c76e22753f0411915d5abeeaa28f
Aureo-Bueno <aureoalexandre@outlook.com>
2025-06-27 10:04:52 -0300

    fix: connection

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,16 @@
 // src/config/database.ts
-import { Sequelize } from 'sequelize';
-import configEnv from './config.env';
-import 'mysql2'; 
+import { Sequelize } from "sequelize";
+import configEnv from "./config.env";
+import "mysql2";
 
 const sequelize = new Sequelize(
-  configEnv.get('DB_NAME') || 'your_database',
-  configEnv.get('DB_USER') || 'your_username',
-  configEnv.get('DB_PASSWORD') || 'your_password',
+  configEnv.get("DB_NAME") || "your_database",
+  configEnv.get("DB_USER") || "your_username",
+  configEnv.get("DB_PASSWORD") || "your_password",
   {
-    host: configEnv.get('DB_HOST') || 'localhost',
-    dialect: 'mysql',
+    host: configEnv.get("DB_HOST") || "localhost",
+    port: Number(configEnv.get("DB_PORT")) || 3306,
+    dialect: "mysql",
   }
 );
 
